a4122ee59114edd0f7e93449cf0088e0a1a19972
DanielLopezr <31810517+DanielLopezr@users.noreply.github.com>
2018-07-23 20:43:12 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+
+	var header = document.getElementsByTagName("header")[0];
+	var altura = header.clientHeight + 10;
+	var padding = altura.toString() + "px";
+	document.getElementById("contenido").style.paddingTop = padding;
