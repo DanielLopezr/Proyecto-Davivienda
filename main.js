7fbce386990fe1f5056a8ac75b7ca4c35c312247
DanielLopezr <31810517+DanielLopezr@users.noreply.github.com>
2018-07-26 11:58:20 -0500

    Pincipal terminado

--- main.js
+++ main.js
@@ -3,8 +3,3 @@ var header = document.getElementsByTagName("header")[0];
 var altura = header.clientHeight;
 var padding = altura.toString() + "px";
 document.getElementsByTagName("body")[0].style.paddingTop = padding;
-
-document.addEventListener('DOMContentLoaded', function() {
-	var elems = document.querySelectorAll('select');
-	var instances = M.FormSelect.init(elems, options);
-});
